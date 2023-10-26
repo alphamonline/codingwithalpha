8f2e7ba6924307b68aa8ff62d01ce8d251976bbd
Alpha <alpha@digemy.com>
2023-10-26 17:14:45 +0200

    update layouts

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
+    <a class="text-gray-500 hover:text-yellow-500" href="/about">About</a>
+    <a class="text-gray-500 hover:text-yellow-500" href="/help">Help</a>
+    <a class="text-gray-500 hover:text-yellow-500" href="/login">Login</a>
+    <a class="text-gray-500 hover:text-yellow-500" href="/explore">Explore</a>
+</footer>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,56 @@
+
+<header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <div id="header-left" class="flex items-center">
+        <div class="text-gray-800 font-semibold">
+            <span class="text-yellow-500 text-xl">&lt;codingwith&gt;</span> alpha
+        </div>
+        <div class="top-menu ml-10">
+            <ul class="flex space-x-4">
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-900 text-sm text-yellow-500"
+                       href="/">
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                       href="/blog">
+                        Blog
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                       href="/about">
+                        About
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                       href="/contact">
+                        Contact
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                       href="/terms">
+                        Terms
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div id="header-right" class="flex items-center md:space-x-6">
+        @guest
+            @include('layouts.partials.header-right-guest')
+        @endguest
+
+        @auth()
+                @include('layouts.partials.header-right-auth')
+        @endauth
+    </div>
+</header>
